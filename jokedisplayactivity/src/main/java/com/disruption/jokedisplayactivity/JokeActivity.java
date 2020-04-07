734e666bc2af8a27e4f08e16f9c5c54f57525a7e
Fbada006 <ferdinandkamata@yahoo.com>
2020-04-07 16:34:49 +0300

    Implement the android library for jokes display

---
+++ jokedisplayactivity/src/main/java/com/disruption/jokedisplayactivity/JokeActivity.java
@@ -0,0 +1,14 @@
+package com.disruption.jokedisplayactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+}
